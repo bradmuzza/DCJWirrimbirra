877004bf3dc339981aae1d2a45b9da7e1e1446b7
bradmuzza <murray_brad@hotmail.com>
2022-11-04 20:05:08 +1100

    UPDATE

--- Python/DB_connection_helper.py
+++ Python/DB_connection_helper.py
@@ -1,9 +1,10 @@
 import pandas as pd
 from sqlalchemy import create_engine
 import json 
-import date
+import codecs
+import datetime 
 
-with open("DB_settings.json") as j:
+with open("DCJWirrimbirra\Python\DB_settings.json") as j:
      config =  json.load(j)
 
 def get_expiry_date():
@@ -18,13 +19,16 @@ def get_engine():
     return eng
 
 def get_source(source_file_path):
-    return pd.read_csv(source_file_path)
+    return pd.read_csv(source_file_path,',')
 
-def load_data_from_csv(source_file_path,table_name,schema,type="append"):
-     df_source = get_source(source_file_path):
+def load_data_from_csv(source_file_path,table_name,schema,type="append"): 
+     df_source = get_source(source_file_path)
      df_source["InsertDate"]= get_insert_date()
      df_source["InsertDate"] = get_expiry_date()
      df_source["MetaSource"] = source_file_path
      eng = get_engine()
      df_source.to_sql(table_name,eng,index=True,if_exists=type, schema=schema)
-    
+
+if __name__ == "__main__":
+    file_path = r'C:\Users\murrayb3\Documents\Old Power Bi Data\Shift Fact Table.csv'
+    load_data_from_csv(file_path,"FactShift","Hist")
